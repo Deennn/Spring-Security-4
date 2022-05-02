d92b89bcd6e742571c9dde9a38bdfcd3e9c94c91
Deennn <abideenlawal70@gmail.com>
2022-05-02 04:34:50 +0100

    initial commit

---
+++ src/main/java/com/deenn/springsecurity4/entity/Student.java
@@ -0,0 +1,15 @@
+package com.deenn.springsecurity4.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+public class Student {
+
+    private Integer studentId;
+    private String studentName;
+}
